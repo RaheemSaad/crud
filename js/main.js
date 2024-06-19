0aca8567edcc7f9823cd92d2b3a244c975a6cf82
raheem <raheemsaad15@gmail.com>
2024-06-19 23:50:54 +0300

    crud

--- js/main.js
+++ js/main.js
@@ -1,53 +1,71 @@
-var ProductNameInput = document.getElementById("name")
-var ProductPriceInput = document.getElementById("price")
-var ProductCategoryInput = document.getElementById("category")
-var ProductDescInput = document.getElementById("desc")
+var bookMark = document.getElementById("nameInput")
+var site = document.getElementById("siteInput")
 
-var allproduct = []
+var allInputs;
 
-function getvalues() {
-    var product = {
-        name: ProductNameInput.value,
-        price: ProductPriceInput.value,
-        category: ProductCategoryInput.value,
-        desc: ProductDescInput.value,
-    };
-    allproduct.push(product)
-    clearinputs()
-    display()    
-    console.log(allproduct);
-
+if (localStorage.getItem("allInputs") != null) {
+    allInputs = JSON.parse(localStorage.getItem("allInputs"))
+    display()
+}
+else {
+    allInputs = [];
 }
 
-function clearinputs() {
-    ProductNameInput.value = ""
-    ProductPriceInput.value = ""
-    ProductCategoryInput.value = ""
-    ProductDescInput.value = ""
-}
-
-function display() {
-    var cartoona = ""
-    for (var i = 0; i < allproduct.length; i++) {
-        cartoona += `
-        <div class="col-md-3 g-5">
-        <div class="bg-info">
-            <img src="./starry-night-over-meadow-with-tree-silhouette-and-mountain-vector.jpg" class="w-100"
-                alt="">
-            <figcaption class="p-2">
-                <h6>Product Name : ${allproduct[i].name}</h6>
-                <h6>Product Price : ${allproduct[i].price}</h6>
-                <h6>Product Category : ${allproduct[i].category}</h6>
-                <h6>Product Desc : ${allproduct[i].desc}</h6>
-                <button class="btn btn-warning">Update</button>
-                <button class="btn btn-danger">Delete</button>
-            </figcaption>
-
-        </div>
-
-    </div>`
+function getValue() {
+    if (validation() == true) {
+        var input = {
+            nameInput: bookMark.value,
+            siteInput: site.value,
+        };
+        allInputs.push(input)
+        localStorage.setItem("allInputs", JSON.stringify(allInputs))
+        display()
+        clearInput()
 
     }
 
-    document.getElementById("demo").innerHTML = cartoona
+}
+
+function clearInput() {
+    bookMark.value = null,
+        site.value = null
+};
+
+
+function display() {
+    var cartoona = ""
+    for ( i = 0; i < allInputs.length; i++) {
+        cartoona += `
+    <tr>
+    <td>${i + 1}</td>
+    <td>${allInputs[i].nameInput}</td>
+    <td><button class="btn btn-success" onclick="visitSite(${i})"><i class="fa-regular fa-eye"></i> Visit</button></td>
+    <td><button class="btn btn-danger" onclick="deleteItem(${i})"><i class="fa-solid fa-trash"></i> Delete</button></td>
+</tr>
+    `
+    }
+    document.getElementById("stock").innerHTML = cartoona;
+
+}
+
+function deleteItem(index) {
+    allInputs.splice(index, 1)
+    localStorage.setItem("allInputs", JSON.stringify(allInputs))
+    display()
+    
+}
+
+function validation() {
+    var regexName = /[A-Z|a-z]{3,20}/
+    var regexSite = /^https/ 
+    if (regexName.test(bookMark.value) == true && regexSite.test(site.value) == true) {
+        return true;
+    }
+    alert("Please enter at least 3 characters and a valid URL stating with https")
+    return false;
+}
+
+function visitSite(index) {
+    open(allInputs[index].siteInput);
+    
 }
